072219fe4847845223b6f3c91a42fb12d310ebb3
pixelsmon <akarshbandi82@gmail.com>
2026-02-04 10:10:45 +0530

    test(config): add firefox and webkit projects to playwright config

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -30,5 +30,17 @@ export default defineConfig({
         },
       },
     },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+      },
+    },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+      },
+    },
   ],
 });
